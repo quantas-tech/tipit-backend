0ad084e444bb639b7c693608056339c19a0561c9
German Llop <germanllop@gmail.com>
2020-08-03 22:40:56 -0500

    deploy

--- routes/takeSurvey.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/',async function(req, res){   
-    res.send('Welcome to the take survey api')
-})
-
-module.exports = router
